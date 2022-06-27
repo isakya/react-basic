4c508db44d6cd5a9ec0fc7e0bd1d0839dc98b3c0
konglong <1016361268@qq.com>
2022-06-27 10:28:26 +0800

    jsx样式控制

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-// 有一个状态 type 1 2 3
-// 1 -> h1
-// 2 -> h2
-// 3 -> h3
+// jsx 样式控制
+// 1. 行内样式 - 在元素身上绑定一个style属性即可
+// 2. 类名样式 - 在元素身上绑定一个className属性即可
+import './app.css'
 
-// 原则：模板中的逻辑尽量保持精简
-// 复杂的多分支逻辑 ：通过一个专门的函数来写分支逻辑，模板只负责调用
 
-const getHtag = (type) => {
-  if (type === 1) {
-    return <h1>this is h1</h1>
-  }
-  if (type === 2) {
-    return <h2>this is h2</h2>
-  }
-  if (type === 3) {
-    return <h3>this is h3</h3>
-  }
+// 抽离样式
+const style = {
+  color: 'red',
+  fontSize: '30px'
 }
 
 function App() {
   return (
     <div className="App">
-      {getHtag(1)}
-      {getHtag(2)}
-      {getHtag(3)}
+      <span style={{
+        color: 'red',
+        fontSize: '30px'
+      }}>this is span</span>
+      <br />
+      <span style={style}>this is span</span>
+      <span className="active">测试类名样式</span>
     </div>
   )
 }
