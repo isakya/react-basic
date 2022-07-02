445a1e401ab436261041f9b34853139a2ecfbf9a
konglong <1016361268@qq.com>
2022-07-02 20:47:44 +0800

    状态修改counter案例

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-// 组件状态 （类组件）
+// 通过类组件修改状态的方式 counter
+
 import React from 'react'
-class TestComponent extends React.Component {
-  // 1. 定义组件状态
+
+class Counter extends React.Component {
+  // 通过state定义组件状态
   state = {
-    // 在这里被定义的各种属性，全都是当前组件的状态
-    name: 'cp teacher'
+    count: 0
   }
   // 事件回调函数
-  changeName = () => {
-    // 3. 修改state中的name
-    // 注意：不可以直接做赋值修改 必须通过一个方法 setState
+  changeCount = () => {
+    // 修改state
+    // react这个体系下 '数据变化' 
+    // value 永远是上一次 count 值 + 1
     this.setState({
-      name: 'zs'
+      count: this.state.count + 1
     })
   }
   render() {
-    // 2. 使用状态
     return (
-      <div>
-        this is TestComponent
-        当前name为: {this.state.name}
-        <button onClick={this.changeName}>修改name</button>
-      </div>
+      <button onClick={this.changeCount}>{this.state.count} click</button>
     )
   }
 }
@@ -29,7 +26,7 @@ class TestComponent extends React.Component {
 function App() {
   return (
     <div>
-      <TestComponent />
+      <Counter></Counter>
     </div>
   )
 }
