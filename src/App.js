7fe3eb4bc90032a5e7179e6d42566f026b7cb504
konglong <1016361268@qq.com>
2022-07-04 05:48:11 +0800

    useState的用法说明

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 // useState 
-// 1. 导入useState函数 （react）
-// 2. 执行这个函数并传入初始值，必须在函数组件中
-// 3. [数据，修改数据的方法]
-// 4. 使用数据 修改数据
+// 状态的读取和修改
+
+// const [count, setCount] = useState(0)
+// 1. useState传过来的参数 作为count的初始值
+// 2. useState返回值是一个数组
+// 3. 名字可以自定义
+// 4. 顺序不可以调换，第一个是状态，第二个是修改状态的方法
+// 5. setCount 函数 作用用来修改count 依旧保持不能直接修改原值
+// 6. setCount(基于原值计算得到新值)
+// 7. count和setCount 是一对，
+
 
 import { useState } from 'react'
 
